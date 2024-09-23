6579b0f46a05d455de775d2c781f75aae816b335
Kareem Magdy <kareemmagdy1175@gmail.com>
2024-09-23 14:57:27 +0300

    EF overview

---
+++ 3.EntityFramework/3.EntityFramework/1.ADO/Overview.cs
@@ -0,0 +1,49 @@
+﻿// Used after install SQL client package. 
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+using System.Configuration;
+
+namespace EntityFramework._1.Overview;
+// What is ADO .NET? 
+// Framework (classes) used to handle relations with databases.
+public class Overview {
+    public static void MainOverview() {
+        SqlConnection sqlCn = new SqlConnection();
+
+        #region Regular connection method
+
+        // // Integrated Security ⇒ true (windows authentication is on)
+        // sqlCn.ConnectionString = "Data Source=.;Initial Catalog=Northwind;Integrated Security = true";
+        //
+        // if (sqlCn.State == ConnectionState.Closed)
+        //     sqlCn.Open();
+        //
+        //
+        // sqlCn.StateChange += (sender, e) => {
+        //     Console.WriteLine($"State Was {e.OriginalState} and changed to {e.CurrentState}");
+        // };
+        // // Occurs when SQL server returns a warning or information message.
+        // sqlCn.InfoMessage += (sender, e) => Console.WriteLine(e.Message);
+        //
+       
+
+        #endregion
+
+
+        // Console.WriteLine();
+
+        #region Using App.config
+
+        sqlCn.ConnectionString = ConfigurationManager.ConnectionStrings["NorthWindCN"].ConnectionString;
+        sqlCn.Open();
+        // Console.Read();
+        sqlCn.Close();
+        Console.WriteLine(ConfigurationManager.AppSettings["BranchID"]);
+        
+
+        #endregion
+    }
+    
+}
---
+++ 3.EntityFramework/3.EntityFramework/1.ADO/SQLCommands.cs
@@ -0,0 +1,36 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EntityFramework._1.Overview;
+
+public class SQLCommands {
+    public static void Main() {
+        SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["NorthWindCN"].ConnectionString);
+        SqlCommand sqlCommand = new SqlCommand(); 
+        
+        sqlConnection.Open();
+
+        sqlCommand.Connection = sqlConnection;
+        // sqlCommand.CommandText = "select count(*) from Products";
+        
+        // ExecuteScalar used for Aggregate functions commands 
+        // int totalProducts = (int)sqlCommand.ExecuteScalar();
+
+        // ExcuteNonQuery a command that doesn't return a set of rows (Insert, delete,....), Just returns rows affected
+        sqlCommand.CommandType = CommandType.StoredProcedure;
+        sqlCommand.CommandText = "UpdateProductNameByProductID";
+        
+        sqlCommand.Parameters.Add("@ProductID", SqlDbType.Int);
+        sqlCommand.Parameters.Add("@ProductName", SqlDbType.NVarChar,40);
+
+        sqlCommand.Parameters["@ProductID"].Value =1 ;
+        sqlCommand.Parameters["@ProductName"].Value = "Chai2";
+
+        // Open SQL server profiler to notice changes ...
+        int rowsAffected = sqlCommand.ExecuteNonQuery();
+
+        Console.WriteLine(rowsAffected);
+        sqlConnection.Close();
+    }
+}
