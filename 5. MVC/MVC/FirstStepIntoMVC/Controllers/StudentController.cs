c9d9e1ca877e29b0251cc1b9beaafd3228e3a451
Kareem Magdy <kareemmagdy1175@gmail.com>
2025-04-24 00:40:21 +0200

    Examples on ViewBag, ViewData and TempData.

--- 5. MVC/MVC/FirstStepIntoMVC/Controllers/StudentController.cs
+++ 5. MVC/MVC/FirstStepIntoMVC/Controllers/StudentController.cs
@@ -8,9 +8,23 @@ public class StudentController : Controller {
     private IStudentRepository _studentRepository;
 
     public StudentController(IStudentRepository studentRepository) {
-        this._studentRepository = studentRepository;
+        _studentRepository = studentRepository;
     }
 
 
-    public IActionResult GetStudentById(int id) => View("index", _studentRepository.GetById(id));
+    public IActionResult GetStudentById(int id) {
+        
+        // this will return "try ViewData" when you call ViewBag.Message or ViewData["message"] 
+        // Because ViewBag is just a wrapper to ViewData "Using dynamic" 
+        
+        // ViewBag and ViewData's lifetime that only spans the current request
+        ViewBag.Message = "Try ViewBag";
+        ViewData["message"] = "Try ViewData";
+
+        // Will return "Try TempData" .
+        // TempData persists across requests, and it's only cleared when you read it.
+        TempData["message"] = "Try TempData";
+
+        return View("index", _studentRepository.GetById(id));
+    }
 }
