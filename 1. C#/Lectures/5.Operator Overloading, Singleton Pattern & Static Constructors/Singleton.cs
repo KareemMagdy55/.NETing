fef9de7c57ce17409919033c8146290ba9d7e2c3
Kareem Magdy <91559979+KareemMagdy55@users.noreply.github.com>
2025-08-11 17:18:49 +0300

    Update Singleton.cs

--- 1. C#/Lectures/5.Operator Overloading, Singleton Pattern & Static Constructors/Singleton.cs
+++ 1. C#/Lectures/5.Operator Overloading, Singleton Pattern & Static Constructors/Singleton.cs
@@ -26,7 +26,7 @@ public class Singleton {
         GraphicsCard gc2 = GraphicsCard.GetCard();
 
 
-        // Hashcode must be the same due the requirement above
+        // Hashcode must be the same
         Console.WriteLine(gc1.GetHashCode());
         Console.WriteLine(gc2.GetHashCode());
 
